de653b08330b4c519aff0f45557df5203eb16979
Ranga Senal Fernando <ranga.fernando@gmail.com>
2022-11-20 14:44:34 +1100

    initial variable

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello to our conference booking application")
+	fmt.Println("Get hear to attend")
+
+	var confierenceName = "Go Conference"
+	fmt.Println(confierenceName)
 }
