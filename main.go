3d47ef4bfbd4ad8ab49c1968c9f60c3b12244038
Ranga Senal Fernando <ranga.fernando@gmail.com>
2022-11-20 19:14:37 +1100

    Coditional loop

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const totalTickets int = 50
+	var ticketsRemains int = totalTickets
+	bookings := []string{}
+
+	for ticketsRemains > 0 {
+		var userName string
+		var userTickets int
+		fmt.Printf("Please enter you First name:\n")
+		fmt.Scan(&userName)
+		fmt.Printf("How many tickets you want to buy?\n")
+		fmt.Scan(&userTickets)
+		bookings = append(bookings, userName)
+
+		ticketsRemains = ticketsRemains - userTickets
+
+		fmt.Printf("Total number of bookings %v \n", len(bookings))
+		fmt.Printf("Total remains tickets %v \n", ticketsRemains)
+
+	}
 }
