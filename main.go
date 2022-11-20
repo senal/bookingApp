4c2eebc4faa8a165092b32fa5dec14297907d3de
Ranga Senal Fernando <ranga.fernando@gmail.com>
2022-11-20 19:52:57 +1100

    Read elements in an array using range

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const totalTickets int = 50
@@ -8,18 +11,32 @@ func main() {
 	bookings := []string{}
 
 	for ticketsRemains > 0 {
-		var userName string
+		var firstName string
+		var lastName string
 		var userTickets int
 		fmt.Printf("Please enter you First name:\n")
-		fmt.Scan(&userName)
+		fmt.Scan(&firstName)
+		fmt.Printf("And your Last name:\n")
+		fmt.Scan(&lastName)
 		fmt.Printf("How many tickets you want to buy?\n")
 		fmt.Scan(&userTickets)
-		bookings = append(bookings, userName)
+		bookings = append(bookings, firstName+" "+lastName)
 
 		ticketsRemains = ticketsRemains - userTickets
 
+		firstNames := []string{}
+
+		// _ in Go is to indicate not used variable
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			var firstName = names[0]
+			firstNames = append(firstNames, firstName)
+		}
+
 		fmt.Printf("Total number of bookings %v \n", len(bookings))
+		fmt.Printf("contains of bookings %v \n", bookings)
 		fmt.Printf("Total remains tickets %v \n", ticketsRemains)
 
+		fmt.Printf("First names of the bookings %v \n", firstNames)
 	}
 }
