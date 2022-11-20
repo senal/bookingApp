23102873cd2dcb9d7cd011515d4e8f9d1e41d15f
Ranga Senal Fernando <ranga.fernando@gmail.com>
2022-11-20 15:19:27 +1100

    data types

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main() {
 	var confierenceName = "Go Conference"
-	var userName = "Ranga"
+	var userName string
+	var userTickets int
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
 	/*
 		how to concatenate, strings
 	*/
+	userName = "Ranga"
+	userTickets = 5
+
 	fmt.Printf("Hey %v, Welcome to %v booking application\n", userName, confierenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, (remainingTickets - userTickets))
 	fmt.Println("Get your tickets here to attend")
+
+	fmt.Printf("User %v, has bought %v tickets", userName, userTickets)
 
 }
